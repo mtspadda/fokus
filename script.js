c12546e6862798464e9e36fd9e90beb0b3bc2f40
Matheus Marques <matheuspadilha.dev@gmail.com>
2024-03-09 13:01:33 -0300

    feat: switch-case

--- script.js
+++ script.js
@@ -8,21 +8,42 @@ const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
 
 //Variaveis de tempo
-const duracaoFoco = 1500; 
-const duracaoDescansoCurto = 300; 
-const duracaoDescansoLongo = 900; 
+const duracaoFoco = 1500;
+const duracaoDescansoCurto = 300;
+const duracaoDescansoLongo = 900;
 
-focoBt.addEventListener('click', ()=>{
-    html.setAttribute('data-contexto', 'foco')
-    banner.setAttribute('src', '/imagens/foco.png')
+alteraContexto = function (contexto) {
+    html.setAttribute('data-contexto', contexto)
+    banner.setAttribute('src', `/imagens/${contexto}.png`)
+    switch (contexto) {
+        case 'foco':
+            titulo.innerHTML = `Otimize sua produtividade,<br>
+            <strong class="app__title-strong">mergulhe no que importa.</strong>
+        `
+            break;
+        case 'descanso-curto':
+            titulo.innerHTML = `Que tal dar uma respirada? <strong class="app__title-strong">Faça uma pausa curta!</strong>
+            ` 
+            break;
+        case 'descanso-longo':
+                titulo.innerHTML = `
+                Hora de voltar à superfície.<strong class="app__title-strong"> Faça uma pausa longa.</strong>
+                `
+            break;
+        default:
+            break;
+    }
+
+}
+
+focoBt.addEventListener('click', () => {
+    alteraContexto('foco')
 })
 
-curtoBt.addEventListener('click', ()=>{
-    html.setAttribute('data-contexto', 'descanso-curto')
-    banner.setAttribute('src', '/imagens/descanso-curto.png')
+curtoBt.addEventListener('click', () => {
+    alteraContexto('descanso-curto');
 })
 
-longoBt.addEventListener('click', ()=>{
-    html.setAttribute('data-contexto', 'descanso-longo')
-    banner.setAttribute('src', '/imagens/descanso-longo.png')
+longoBt.addEventListener('click', () => {
+    alteraContexto('descanso-longo');
 })
